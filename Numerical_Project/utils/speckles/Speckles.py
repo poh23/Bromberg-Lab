4021fd801c74d3e0939e1ae9eeedcddd40c27f97
noalevy42 <55984594+noalevy42@users.noreply.github.com>
2025-08-04 13:42:08 +0300

    better noa

--- Numerical_Project/utils/speckles/Speckles.py
+++ Numerical_Project/utils/speckles/Speckles.py
@@ -12,19 +12,12 @@ matplotlib.use('TKAgg')
 
 
 class Speckles:
-
+    
     MICRON = 1e-6
     THIN_DIFFUSER =1
     THICK_DIFFUSER =2
-    Nx = 1024
-    Ny = 1024
-    Dx = 3e-3 # 3mm
-    Dy = 3e-3 # 3mm
-    dx = Dx / Nx
-    dy = Dy / Ny # pixel size 3 micron
     wl = 808e-9 # Wavelength nanometer
     sig = 700e-6 # of the Gaussian 700 micron
-
     L = 20e-3 # 20 mm 
     # L = 10e-3 
     theta = 0.02 # Desired Scattering angle
@@ -32,38 +25,46 @@ class Speckles:
     active_slice = np.index_exp[450:650, 450:650]
     roi =  np.index_exp[509:512, 509:512]
     
-    # init grid
-    X = (np.arange(1, Nx + 1) - (Nx / 2 + 0.5)) * dx
-    Y = (np.arange(1, Ny + 1) - (Ny / 2 + 0.5)) * dy
-    XX, YY = np.meshgrid(X, Y)
-
-    # defing k-space
-    fs = 1 / (XX.max() - XX.min())  # sapatial freq = 1/x
-    freq_x = fs * np.arange(-Nx // 2, Nx // 2)
-    fs = 1 / (YY.max() - YY.min())
-    freq_y = fs * np.arange(-Ny // 2, Ny // 2)
-    # np.fft.fftfreq does this automatically for you 
-    freq_XXs, freq_YYs = np.meshgrid(freq_x, freq_y)
-    light_k = 2 * np.pi / wl
-    k_xx = freq_XXs * 2 * np.pi
-    k_yy = freq_YYs * 2 * np.pi
-    k_z_sqr = light_k ** 2 - (k_xx ** 2 + k_yy ** 2)
-    # Remove all the negative component, as they represent evanescent waves, see Fourier Optics page 58
-    np.maximum(k_z_sqr, 0, out=k_z_sqr)
-    k_z = np.sqrt(k_z_sqr)
-
-    def __init__(self) -> None:
+    def __init__(self, Nx, Ny, Dx, Dy) -> None:
         self.diffuser1_phase = None
         self.diffuser2_phase = None
         self.slm_phase = None
         self.E_in = None
         self.is_thick: bool = None
         self.thick_diffuser_width = None
+        self.Nx = Nx
+        self.Ny = Ny
+        self.Dx = Dx
+        self.Dy = Dy
+        self.dx = self.Dx / self.Nx
+        self.dy = self.Dy / self.Ny # pixel size 3 micron
+    
+        
+        # init grid
+        X = (np.arange(1, Nx + 1) - (Nx / 2 + 0.5)) * self.dx
+        Y = (np.arange(1, Ny + 1) - (Ny / 2 + 0.5)) * self.dy
+        self.XX, self.YY = np.meshgrid(X, Y)
+
+        # defing k-space
+        fs = 1 / (self.XX.max() - self.XX.min())  # sapatial freq = 1/x
+        freq_x = fs * np.arange(-Nx // 2, Nx // 2)
+        fs = 1 / (self.YY.max() - self.YY.min())
+        freq_y = fs * np.arange(-Ny // 2, Ny // 2)
+        # np.fft.fftfreq does this automatically for you 
+        freq_XXs, freq_YYs = np.meshgrid(freq_x, freq_y)
+        light_k = 2 * np.pi / Speckles.wl
+        k_xx = freq_XXs * 2 * np.pi
+        k_yy = freq_YYs * 2 * np.pi
+        k_z_sqr = light_k ** 2 - (k_xx ** 2 + k_yy ** 2)
+        # Remove all the negative component, as they represent evanescent waves, see Fourier Optics page 58
+        np.maximum(k_z_sqr, 0, out=k_z_sqr)
+        self.k_z = np.sqrt(k_z_sqr)
+
 
     def _get_prop_mat(self,l,rev=False):
         if rev == True:
-            return np.exp(-1j * SanityClass.k_z * l)
-        return np.exp(+1j * SanityClass.k_z * l)
+            return np.exp(-1j * self.k_z * l)
+        return np.exp(+1j *  self.k_z * l)
 
     def _my_fft2(self,E):
         return np.fft.fftshift(pyfftw.interfaces.numpy_fft.fft2(
@@ -75,10 +76,10 @@ class Speckles:
             np.fft.fftshift(E_K), overwrite_input=False, auto_align_input=True)
     )
 
-    def phase_mask_macro_pixels_method(self,XX, macro_pixel_size)->np.ndarray:
-        init_mask = np.random.uniform(0, 2*np.pi, size=(SanityClass.Nx//macro_pixel_size, SanityClass.Ny//macro_pixel_size))
+    def phase_mask_macro_pixels_method(self, macro_pixel_size)->np.ndarray:
+        init_mask = np.random.uniform(0, 2*np.pi, size=( self.Nx//macro_pixel_size,  self.Ny//macro_pixel_size))
         # resize: start with 256X256 random, and then resize to 1024X1024 for macro pixels 
-        phase_mask = cv2.resize(init_mask, XX.shape[::-1], interpolation=cv2.INTER_AREA)
+        phase_mask = cv2.resize(init_mask, self.XX.shape[::-1], interpolation=cv2.INTER_AREA)
         return phase_mask
 
 
@@ -112,14 +113,14 @@ class Speckles:
         return E_out_fsp,E_out_fur
     
     def propagate_thick_with_angle(self,E_start,angle):
-        E_init = E_start.copy()*np.exp(1j*angle*(SanityClass.XX+SanityClass.YY))
+        E_init = E_start.copy()*np.exp(1j*angle*(self.XX+self.YY))
         E_init /= np.sqrt(np.sum(np.abs(E_init**2)))
         E_slm = E_init.copy()*np.exp(1j*self.slm_phase)
         E_diffuser1 = E_slm*np.exp(1j*self.diffuser1_phase)
         E = self.free_space_propagation(E_diffuser1,self.thick_diffuser_width)
         E_diffuser2 = E* np.exp(1j*self.diffuser2_phase)
         E_out_fsp = self.free_space_propagation(E_diffuser2,self.L)
-        E_clear = E_diffuser2/np.exp(1j*angle*(SanityClass.XX+SanityClass.YY))
+        E_clear = E_diffuser2/np.exp(1j*angle*( self.XX+ self.YY))
         E_out_fur = self._my_fft2(E_clear)
         return E_out_fsp,E_out_fur
     
@@ -134,7 +135,7 @@ class Speckles:
     def calc_one_thick_diff_mem(self,d):
         self.thick_diffuser_width =d #m
         # E_start = np.exp(-(SanityClass.XX**2+SanityClass.YY**2)/(SanityClass.sig)**2)
-        E_start = np.exp(-(SanityClass.XX**2+SanityClass.YY**2)/(SanityClass.sig/30)**2) #the sigma of the focus i want to achieve - nack prop from here to find slm phase
+        E_start = np.exp(-(self.XX**2+ self.YY**2)/( self.sig/30)**2) #the sigma of the focus i want to achieve - nack prop from here to find slm phase
 
         E_init = E_start/np.sqrt(np.sum(np.abs(E_start**2)))
         E_p = self.propagate_reverse_thick_f(E_init)
@@ -145,8 +146,8 @@ class Speckles:
         r= np.pi/150
         phase_vec = np.linspace(0,r,30)
         for i,phase in enumerate(phase_vec):
-            a = (2*np.pi/SanityClass.wl)*np.sin(phase)
-            E_start = np.exp(-(SanityClass.XX**2+SanityClass.YY**2)/(SanityClass.sig/5)**2) #the sigma of the laser, this one is important to speckle grain
+            a = (2*np.pi/ Speckles.wl)*np.sin(phase)
+            E_start = np.exp(-( self.XX**2+self.YY**2)/(self.sig/5)**2) #the sigma of the laser, this one is important to speckle grain
             E, E_fur = self.propagate_thick_with_angle(E_start,a)
             E_fur/= np.sqrt((np.abs(E_fur)**2).sum())
             max_speck = E_fur[ind_row-l:ind_row+l+1,ind_col-l:ind_col+l+1]
@@ -170,7 +171,7 @@ class Speckles:
             E_out = self.propagate_thin(E_init)
         if self.is_thick == True:
             E_out = self.propagate_thick(E_init)
-        I = np.sum(np.abs(E_out[SanityClass.roi])**2)
+        I = np.sum(np.abs(E_out[Speckles.roi])**2)
         return I
 
     def find_best_phi(self,A, i, j,phase_vec,E_init):
@@ -178,7 +179,7 @@ class Speckles:
         for ind, phase in enumerate(phase_vec):
             A[i,j] = phase
             A_scaled = cv2.resize(A, (200,200), interpolation=cv2.INTER_AREA)
-            self.slm_phase[SanityClass.active_slice] = A_scaled
+            self.slm_phase[Speckles.active_slice] = A_scaled
             Is[ind] = self.get_cost(E_init)
         CC = (Is * np.exp(1j * phase_vec)).sum()
         best_phi = np.mod(np.angle(CC)+2*np.pi, 2*np.pi)
